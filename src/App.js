a7744d6373eaa9b9ae5799f255c024121b69cc78
Baijayanti Dash <dash.baijayanti@gmail.com>
2024-10-14 08:17:40 +0200

    docs:

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import EventList from './components/EventList';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+ return (
+   <div className="App">
+     <div id="event-list"></div>
+   </div>
+ );
 }
 
 export default App;
+
+
