cd409745156251fd03f281eef0872f0ea829fe2f
Baijayanti Dash <dash.baijayanti@gmail.com>
2024-10-21 13:19:57 +0200

    docs:

--- src/api.js
+++ src/api.js
@@ -73,7 +73,7 @@ export const getAccessToken = async () => {
     const code = await searchParams.get("code");
       if (!code) {
         const response = await fetch(
-          "https://m4smi1kcse.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
+          "https://m4smi1kcse.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url",{mode: 'no-cors'}
         );
         const result = await response.json();
         const { authUrl } = result;
