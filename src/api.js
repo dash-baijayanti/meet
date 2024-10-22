346de4d4dfe18270a08b1bea9b38be9f4b7b0ac4
Baijayanti Dash <dash.baijayanti@gmail.com>
2024-10-22 10:35:39 +0200

    d

--- src/api.js
+++ src/api.js
@@ -74,7 +74,7 @@ export const getAccessToken = async () => {
     const code = await searchParams.get("code");
       if (!code) {
         const response = await fetch(
-          "https://cctxyvyeul.execute-api.eu-central-1.amazonaws.com/api/get-auth-url",{mode: 'no-cors'}
+          "https://cctxyvyeul.execute-api.eu-central-1.amazonaws.com/api/get-auth-url"
         );
         const result = await response.json();
         const { authUrl } = result;
